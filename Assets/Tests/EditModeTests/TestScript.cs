264c9ab4c4ac2d896b6867aaf71d1e812b1f5d21
Xiaolei Zhu <xiaolei2004@gmail.com>
2025-11-06 19:14:23 +0100

    Preparacion para escribir tests

--- Assets/Tests/EditModeTests/TestScript.cs
+++ Assets/Tests/EditModeTests/TestScript.cs
@@ -12,13 +12,4 @@ public class TestScript
         // Use the Assert class to test conditions
     }
 
-    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
-    // `yield return null;` to skip a frame.
-    [UnityTest]
-    public IEnumerator TestScriptWithEnumeratorPasses()
-    {
-        // Use the Assert class to test conditions.
-        // Use yield to skip a frame.
-        yield return null;
-    }
 }
