2c56ccf2d6c777604f88e97021b674498d8a36be
Qiu2025 <siyuanqiu123@gmail.com>
2025-11-27 23:00:43 +0100

    Revert commit changes
    Checkout ac4e3d3dc7441438ede8c7c53157c95bb771927d

---
+++ Assets/Scripts/EfectoTitulo.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections; 
+
+public class EfectoTitulo : MonoBehaviour
+{
+    public CanvasGroup canvasGroup; 
+    public float tiempoAparicion = 1.5f;
+    public float tiempoEspera = 3.0f;
+    public float tiempoDesaparicion = 1.5f;
+
+    void Start()
+    {
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0;
+            StartCoroutine(SecuenciaTitulo());
+        }
+        else
+        {
+            Debug.LogError("Falta asignar el CanvasGroup en el inspector");
+        }
+    }
+
+    IEnumerator SecuenciaTitulo()
+    {
+        float timer = 0f;
+        while (timer < tiempoAparicion)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(0f, 1f, timer / tiempoAparicion);
+            yield return null;
+        }
+        canvasGroup.alpha = 1f;
+
+      
+        yield return new WaitForSeconds(tiempoEspera);
+
+  
+        timer = 0f;
+        while (timer < tiempoDesaparicion)
+        {
+            timer += Time.deltaTime;
+            canvasGroup.alpha = Mathf.Lerp(1f, 0f, timer / tiempoDesaparicion);
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+        gameObject.SetActive(false);
+    }
+}
