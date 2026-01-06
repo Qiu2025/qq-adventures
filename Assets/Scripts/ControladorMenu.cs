4ed33412383fc9c3770f3dc23668bffe3158d3ff
Qiu2025 <siyuanqiu123@gmail.com>
2026-01-06 19:02:30 +0100

    BGM added

--- Assets/Scripts/ControladorMenu.cs
+++ Assets/Scripts/ControladorMenu.cs
@@ -6,7 +6,9 @@ public class ControladorMenu : MonoBehaviour
 
     void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) 
+            && !Input.GetMouseButton(2) && !Input.GetMouseButton(3) && !Input.GetMouseButton(4) 
+            && !Input.GetMouseButton(5) && !Input.GetMouseButton(6))
         {
             CambiarDeEscena();
         }
