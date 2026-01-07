b3ced122bba37a1ad513b3e2e92b3cd7105ee63a
Qiu2025 <siyuanqiu123@gmail.com>
2026-01-07 19:24:01 +0100

    Level transition added

--- Assets/Scripts/EfectoTitulo.cs
+++ Assets/Scripts/EfectoTitulo.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-using System.Collections; 
+using System.Collections;
 
 // Script para mostrar el titulo "NIVEL 1: DESIERTO"
 public class EfectoTitulo : MonoBehaviour
 {
     public CanvasGroup canvasGroup; 
+    public float espera = 1f;
     public float tiempoAparicion = 1.5f;
     public float tiempoEspera = 3.0f;
     public float tiempoDesaparicion = 1.5f;
@@ -24,6 +25,8 @@ public class EfectoTitulo : MonoBehaviour
 
     IEnumerator SecuenciaTitulo()
     {
+        yield return new WaitForSeconds(espera);
+
         float timer = 0f;
         while (timer < tiempoAparicion)
         {
@@ -32,11 +35,9 @@ public class EfectoTitulo : MonoBehaviour
             yield return null;
         }
         canvasGroup.alpha = 1f;
-
       
         yield return new WaitForSeconds(tiempoEspera);
 
-  
         timer = 0f;
         while (timer < tiempoDesaparicion)
         {
--- Assets/Scripts/Puerta.cs
+++ Assets/Scripts/Puerta.cs
@@ -1,16 +1,35 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-// Script de las puertas de paso de nivel
+// Script de las puertas, para reproducir una animacion y cargar nueva escena
 public class Puerta : MonoBehaviour
 {
-    public string sceneName;
+    [SerializeField] private Animator animator;
+    [SerializeField] private float transitionTime = 1f;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(sceneName);
+            int nextIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+            if (nextIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                StartCoroutine(LoadNextLevel(nextIndex));   // Siguiente nivel
+            } else
+            {
+                StartCoroutine(LoadNextLevel(0));   // Si es el ultimo, al menu
+            }
         }
+    }
+
+    IEnumerator LoadNextLevel(int levelIndex)
+    {
+        animator.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(levelIndex);
     }
 }
